f7155eedaa3d0a9d4bc88b6ce9e81cd9717a5e5a
Isaac Sparling <isaac@imbp.local>
2023-01-18 17:38:10 -0800

    :lock_with_ink_pen: add gitconfig + emoji script

--- install.sh
+++ install.sh
@@ -6,5 +6,9 @@ cp .zshrc ~/.zshrc
 echo ".zshrc copied to ~/.zshrc"
 cp .git-prompt.sh ~/.git-prompt.sh
 echo ".git-prompt.sh copied to ~/.git-prompt.sh"
+cp emoji.sh /usr/local/bin/emoji
+echo "random emoji script installed at [/usr/local/bin/emoji]"
+cp .gitconfig ~/.gitconfig
+echo ".gitconfig copied to ~/.gitconfig"
 
 echo "Please restart your shell"
