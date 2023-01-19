427fd9e6215f5b0683b326ac466330f66689fc17
Isaac Sparling <223606+isparling@users.noreply.github.com>
2023-01-19 09:54:34 -0800

    :bulb: actually restore vscode extensions

--- vscode/restore.sh
+++ vscode/restore.sh
@@ -1,3 +1,4 @@
 cd ~/_dotfiles/vscode
 cp artifacts/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 cp artifacts/settings.json ~/Library/Application\ Support/Code/User/settings.json
+zsh artifacts/restore-vscode-extensions.sh
